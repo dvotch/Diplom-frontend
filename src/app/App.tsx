62f091fa2447bac9dcb25687d237b3fb7598c627
Igor Titov <god19842@mail.ru>
2024-04-15 21:27:27 +0300

    authorazitoain

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,67 @@
-function App() {
-  return <>Hello world</>;
-}
+import { useState, ChangeEvent, FormEvent } from "react";
 
-export default App;
+const AuthForm = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    fetch("http://localhost:3001/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => console.log(response))
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="login">
+            Логин:
+            <input
+              name="email"
+              type="text"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="password">
+            Пароль:
+            <input
+              name="password"
+              type="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <button type="submit">Авторизироваться</button>
+      </form>
+    </div>
+  );
+};
+
+export default AuthForm;
