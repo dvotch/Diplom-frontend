6486a1b7fe67d8e1a47f3804fd233d69805f087e
dvotch <dvotch@mail.ru>
2024-04-20 17:55:51 +0300

    add 404 page and Statement widget with full logic

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,16 +1,19 @@
-import { createBrowserRouter } from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import { RootLayot } from "./layout/Root";
 import { Statement } from "../widgets/statement/ui";
+import { NotFound } from "../pages/404/NotFound";
 
-export const route = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootLayot />,
-    children: [
-      {
-        path: "statement",
-        element: <Statement />,
-      },
-    ],
-  },
-]);
+export const route = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route element={<RootLayot />} path="/">
+        <Route element={<Statement />} index path="statement" />
+      </Route>
+      <Route element={<NotFound />} path="*" />
+    </Route>
+  )
+);
---
+++ src/widgets/statement/components/index.tsx
@@ -0,0 +1,2 @@
+export * from "./QuaterButtons";
+export * from "./LessonButtons";
