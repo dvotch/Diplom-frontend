37a187055ac3518aa026b407c885c5d5d04cb8fb
dvotch <dvotch@mail.ru>
2024-04-23 12:42:59 +0300

    some fix

--- src/widgets/aside/api/logo.ts
+++ src/widgets/aside/api/logo.ts
@@ -3,7 +3,7 @@ import { STUDENT_TOKEN } from "../../../shared/const";
 import { useQuery } from "@tanstack/react-query";
 
 export const getLogo = async () => {
-  return await axios.get<string>("http://localhost:3001/api/user/logo", {
+  return await axios.get<string>("http://prod.dvotch.ru:3001/api/user/logo", {
     headers: {
       Authorization: STUDENT_TOKEN,
     },
--- src/widgets/settings/api/logo.ts
+++ src/widgets/settings/api/logo.ts
@@ -5,7 +5,7 @@ import { STUDENT_TOKEN } from "../../../shared/const";
 export const uploadLogo = (file: Blob) => {
   const formData = new FormData();
   formData.append("file", file);
-  return axios.post("http://localhost:3001/api/user/logo", formData, {
+  return axios.post("http://prod.dvotch.ru:3001/api/user/logo", formData, {
     headers: {
       "Content-Type": `multipart/form-data`,
       Authorization: STUDENT_TOKEN,
