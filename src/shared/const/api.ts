b37285aef0f03b3377e1e63be6146808e7f5abbd
dvotch <dvotch@mail.ru>
2024-04-25 15:05:18 +0300

    add portfolio forn and api constants

---
+++ src/shared/const/api.ts
@@ -0,0 +1,10 @@
+export const BASE_URL = "http://prodd.dvotch.ru:3001/api";
+
+export const CATEGORIES_URL = BASE_URL + "/category";
+export const PORTFOLIO_URL = BASE_URL + "/portfolio";
+
+export const STUDENT_URL = BASE_URL + "/student";
+export const STUDENT_STATEMENT_URL = STUDENT_URL + "/statement";
+
+export const TEACHER_URL = BASE_URL + "/teacher";
+export const TEACHER_STATEMENT_URL = TEACHER_URL + "/statement";
--- src/widgets/achievementForm/ui.tsx
+++ src/widgets/achievementForm/ui.tsx
@@ -1,30 +1,52 @@
-import clsx from "clsx";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { years } from "./config/years";
+import { useEffect, useState } from "react";
+import { useGetCategories } from "./api/categories";
+import { useUploadPortfolio } from "./api/postPortfolio";
+import { useQueryClient } from "@tanstack/react-query";
+import { decodeJwt } from "../../shared/helpers/decodeJwt";
 
-type Inputs = {
+export type Inputs = {
   name: string;
   year: number;
   categoryId: number;
-  photo: File;
+  photo: FileList | Blob;
 };
 
 export const AchievementForm = () => {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm<Inputs>();
+  const { register, handleSubmit } = useForm<Inputs>();
 
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  const [fileName, setFileName] = useState("");
+  const { data: categories } = useGetCategories();
+  const queryClient = useQueryClient();
+  const { mutate: postPortfolio } = useUploadPortfolio(queryClient);
+
+  useEffect(() => {
+    const inputFileText = document.querySelector(
+      "input[type=file]"
+    ) as HTMLInputElement;
+    inputFileText.addEventListener("change", (e: Event) => {
+      if (inputFileText.files && inputFileText.files[0])
+        setFileName(inputFileText.files[0].name);
+    });
+  }, []);
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    if (data.photo instanceof FileList) {
+      data.photo = new Blob([data.photo[0]], { type: "image/png" });
+      const userId = decodeJwt().id;
+      postPortfolio({ userId, ...data });
+    }
+
+    console.log(data);
+  };
 
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="mt-11 flex flex-col gap-6 text-xl"
+      className="mt-11 flex flex-col gap-6 text-xl w-[600px]"
     >
-      <label className="w-[600px]">
+      <label className="">
         Название файла:
         <br />
         <input
@@ -42,7 +64,7 @@ export const AchievementForm = () => {
               Выберите из списка
             </option>
             {years.map((year) => (
-              <option value={year}>{year}</option>
+              <option value={+year}>{year}</option>
             ))}
           </select>
         </div>
@@ -54,18 +76,29 @@ export const AchievementForm = () => {
             <option selected disabled value="" hidden>
               Выберите из списка
             </option>
-            <option>Выберите из списка</option>
-            <option>Выберите из списка</option>
+            {categories &&
+              categories.map((category) => (
+                <option value={category.id} key={category.id}>
+                  {category.name}
+                </option>
+              ))}
           </select>
         </div>
       </label>
-      <label className="input-file">
-        Загрузить фотографию
+      <label className="input-file w-[400px]" htmlFor="file">
+        Загрузить фотографию:
         <br />
-        <span className="input-file-text"></span>
-        <input type="file" {...register("photo")} />
-        <span className="input-file-btn">Выберите файл</span>
+        <div className="mt-2 flex">
+          <span className="input-file-text flex-1 ">
+            <div className="w-[95%] text-nowrap overflow-hidden">
+              {fileName}
+            </div>
+          </span>
+          <input type="file" className="" id="file" {...register("photo")} />
+          <span className="input-file-btn">Выберите файл</span>
+        </div>
       </label>
+      <button type="submit">submit</button>
     </form>
   );
 };
